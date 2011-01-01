54bcc905d50e3eb81711d178897d4118a857fac2
Eric Chiu <echiu@imservice.com>
2011-01-01 13:03:59 -0800

    Revert "Merge branch 'master' of github.com:erictchiu/feedadmin"
    This reverts commit 08f04abcbab3e5f0fce03459def75479c644546f, reversing
    changes made to d0d9ed8537c5b2f8c1f42505c272ee2572de3a2a.

--- talkback/install/info.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Browsing to this script will display information about your PHP and server settings
-phpinfo();
-?>
--- talkback/language/my-english.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	Insert your language file overrides into this file. The primary language file(s) may change
-	in future releases.
-	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*/
-
-// replace this line with your first entry
-?>
